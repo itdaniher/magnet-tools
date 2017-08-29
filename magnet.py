9c819b3a9db3c25a9de35e4345d4c18c0a1c3ebf
Ian Daniher <itdaniher@gmail.com>
2017-08-29 02:31:04 +0000

    periodic status updates

--- magnet.py
+++ magnet.py
@@ -23,9 +23,10 @@ with open(sys.argv[1], 'r+') as f:
         i += 1
         line = data.readline()
         res = urllib.parse.parse_qs(line[lm:-1].decode('utf-8'))
+        if i % 1000 == 0:
+            print(i)
         try:
             writer.add_document(title=res["dn"][0] + " " + res["xt"][0], content=line.strip().decode('utf-8'), infohash=res["xt"][0])
         except:
             break
-    print(i)
 writer.commit()
